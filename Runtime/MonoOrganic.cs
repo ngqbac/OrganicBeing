9b5910545d456b5d68a0113b0cf0eb98f02df89b
Bacnq's MPB <ngqbac@gmail.com>
2025-09-21 20:39:42 +0700

    Implement MonoOrganic

---
+++ Runtime/MonoOrganic.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace OrganicBeing
+{
+    public abstract class MonoOrganic: MonoBehaviour, IOrganic
+    {
+        protected virtual void Awake() => Grow();
+
+        public bool IsReady { get; set; }
+        public void Grow()
+        {
+            if (IsReady) return;
+            OnGrow();
+            IsReady = true;
+        }
+        
+        protected abstract void OnGrow();
+        
+        protected void WhenReady(Action action)
+        {
+            if (!IsReady) Grow();
+            action();
+        }
+
+        protected T WhenReady<T>(Func<T> func)
+        {
+            if (!IsReady) Grow();
+            return func();
+        }
+
+        public virtual void OnRecycle() { }
+    }
+}
